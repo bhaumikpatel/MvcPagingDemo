0b26eb8263738af7bde5187c23e7ee8a66beb4ab
Bhaumik Patel <bhaumik1008@yahoo.com>
2013-06-15 22:33:36 +0530

    Start

---
+++ MvcPagingDemo/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections;
+
+namespace MvcPagingDemo.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+    }
+}
